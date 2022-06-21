ab3892956a72d52496a5fcb1ce7ba9ffc8848a44
gmgwhDM2005 <107148267+gmgwhDM2005@users.noreply.github.com>
2022-06-21 21:36:19 +0100

    Create f string.py

---
+++ python/General/f string.py
@@ -0,0 +1,9 @@
+#Welcome to f string
+#When we set a variable, we can output it using this as a method
+
+#lets take name for example
+#we set name as an input string
+name=input('What is your name?')
+
+#We then print this into an f string, the variable must be surrounded by {}
+print(f'{name} is your name')
