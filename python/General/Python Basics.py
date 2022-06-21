c0e225d1b19a40878068b91beb775f5acd9be260
gmgwhDM2005 <107148267+gmgwhDM2005@users.noreply.github.com>
2022-06-21 21:37:07 +0100

    Create Python Basics.py

---
+++ python/General/Python Basics.py
@@ -0,0 +1,10 @@
+#Random Number Generator
+
+#Imported random from the py library
+import random
+
+#Set a variable with a ran num between 1,6
+randNum=random.randint(1, 6)
+
+#Output the ran num
+print(str(randNum) + ' has been generated'))
