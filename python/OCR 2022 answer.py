6079d260be548f944c9eff533fe17ff19bd18e23
gmgwhDM2005 <107148267+gmgwhDM2005@users.noreply.github.com>
2022-06-21 12:11:29 +0100

    Delete OCR 2022 answer.py

--- python/OCR 2022 answer.py
+++
@@ -1,9 +0,0 @@
-loop = input('Num of times in loop')
-array=[]
-for i in range(int(loop)):
-    num=int(input('Num:'))
-    array.append(num)
-total=sum(array)
-average=total/len(array)
-print('Total amount: ' + str(total))
-print('Average: ' + str(average))
