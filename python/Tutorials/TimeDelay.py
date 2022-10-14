a769a8f9016f859070444db4a7ee3037bce2f1a5
gmgwhDM2005 <107148267+gmgwhDM2005@users.noreply.github.com>
2022-10-14 10:01:51 +0100

    Create TimeDelay.py

---
+++ python/Tutorials/TimeDelay.py
@@ -0,0 +1,11 @@
+#Import the "time" module as "t"
+import time as t
+
+#Print out a message
+print("Hello")
+#Add 1 second of delay
+t.sleep(1)
+print("World!")
+
+#The number one can be changed to increase
+#Or decrease the amount of time that the program/algorithm is put to sleep
