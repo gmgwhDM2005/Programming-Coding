a8f7fad7ff224d91c054fa0e31d25af98806aad7
gmgwhDM2005 <107148267+gmgwhDM2005@users.noreply.github.com>
2022-11-01 12:02:44 +0000

    Create IsInstance.py

---
+++ python/Tutorials/IsInstance.py
@@ -0,0 +1,10 @@
+#Created an array assigned to the variable p
+p=['a','b']
+
+#If state testing if p IS a str
+if isinstance(p, str)==True:
+    #If this is true, it will print true
+    print('True')
+#If false, it will print false (which it wont be as there are no other data types)
+else:
+    print('False')
