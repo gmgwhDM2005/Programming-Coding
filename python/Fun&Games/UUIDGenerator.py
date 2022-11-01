51a8787ab02f1f72cb086f6b2dde5c84913a2270
gmgwhDM2005 <107148267+gmgwhDM2005@users.noreply.github.com>
2022-11-01 12:00:17 +0000

    Update UUIDGenerator.py

--- python/Fun&Games/UUIDGenerator.py
+++ python/Fun&Games/UUIDGenerator.py
@@ -1,6 +1,41 @@
-import uuid
+import uuid as u, time as t
+while True:
+     while True:
+          loop=input('How many UUID\'s would you like to create? ')
+          break
+          try:
+               int(loop)
+               break
+          except:
+               print('Please input a number (int)')
+     loop=int(loop)
+     if loop<=500:
+          break
+     else:
+          print('The max amount of UUID\'s is 500.')
 
-loop=input('How many uuid\'s would you like generated?')
+TextToF=str(input('Would you like to download these UUID\'s (y/n)? '))
+def uuidGen():
+     uuid=u.uuid4()
+     return uuid
+if TextToF.lower()=='y':
+     file=open("GenUUIDs.txt", "w")
+     for i in range(loop):
+          file.write(f"{uuidGen()}\n")
+          if i == loop/2:
+               t.sleep(1)
+               print('Halfway Downloaded..')
+               t.sleep(1)
+     file.close()
+     print('Success!')
+     print('Downloaded GenUUIDs.txt')
+else:
+     for i in range(loop):
+          print(f'[{i+1}]{uuidGen()}')
 
-for i in range(int(loop)):
-     print(i+1, uuid.uuid4())
+
+     
+          
+     
+
+
